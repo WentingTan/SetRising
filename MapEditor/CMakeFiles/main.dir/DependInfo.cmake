bd745a9d496f6e20fb3fc3c04dbc086f317decec
Wenting Tan <wwtan@email.wm.edu>
2014-11-10 15:58:17 -0500

    New Map

---
+++ MapEditor/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/bin/main.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/bin/main.cpp.o"
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/src/Button.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/src/Button.cpp.o"
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/src/EditState.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/src/EditState.cpp.o"
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/src/HScrollBar.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/src/HScrollBar.cpp.o"
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/src/Input.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/src/Input.cpp.o"
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/src/LoadMenu.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/src/LoadMenu.cpp.o"
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/src/MainMenu.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/src/MainMenu.cpp.o"
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/src/MapEditor.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/src/MapEditor.cpp.o"
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/src/MapPane.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/src/MapPane.cpp.o"
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/src/NewMenu.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/src/NewMenu.cpp.o"
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/src/OutputPane.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/src/OutputPane.cpp.o"
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/src/RadioButton.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/src/RadioButton.cpp.o"
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/src/TextField.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/src/TextField.cpp.o"
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/src/Tile.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/src/Tile.cpp.o"
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/src/TileInfoPane.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/src/TileInfoPane.cpp.o"
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/src/TileMap.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/src/TileMap.cpp.o"
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/src/TilePane.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/src/TilePane.cpp.o"
+  "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/src/VScrollBar.cpp" "/Users/wenting/Documents/CSci/GameDesign/GitHub/SetRising/MapEditor/CMakeFiles/main.dir/src/VScrollBar.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Library/Frameworks/SFML.framework"
+  "include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ MapEditor/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/Button.cpp.o"
+  "CMakeFiles/main.dir/src/EditState.cpp.o"
+  "CMakeFiles/main.dir/src/HScrollBar.cpp.o"
+  "CMakeFiles/main.dir/src/Input.cpp.o"
+  "CMakeFiles/main.dir/src/LoadMenu.cpp.o"
+  "CMakeFiles/main.dir/src/MainMenu.cpp.o"
+  "CMakeFiles/main.dir/src/MapEditor.cpp.o"
+  "CMakeFiles/main.dir/src/MapPane.cpp.o"
+  "CMakeFiles/main.dir/src/NewMenu.cpp.o"
+  "CMakeFiles/main.dir/src/OutputPane.cpp.o"
+  "CMakeFiles/main.dir/src/RadioButton.cpp.o"
+  "CMakeFiles/main.dir/src/TextField.cpp.o"
+  "CMakeFiles/main.dir/src/Tile.cpp.o"
+  "CMakeFiles/main.dir/src/TileInfoPane.cpp.o"
+  "CMakeFiles/main.dir/src/TileMap.cpp.o"
+  "CMakeFiles/main.dir/src/TilePane.cpp.o"
+  "CMakeFiles/main.dir/src/VScrollBar.cpp.o"
+  "CMakeFiles/main.dir/bin/main.cpp.o"
+  "main.pdb"
+  "main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
